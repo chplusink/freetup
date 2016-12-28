865b7132b8f7c9199109ffc1b448d19f6e0c4cc0
Vic Lam <lam.vic.k@gmail.com>
2016-12-28 01:54:48 -0500

    add filtering by language and zip code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :events, only: [:index]
-  resources :meetup_requests, only: [:new]
+  resources :meetup_requests, only: [:index, :new]
 
   post '/meetup_requests', to: "meetup_requests#new_meetup_request"
 
--- db/migrate/002_create_meetup_requests.rb
+++ db/migrate/002_create_meetup_requests.rb
@@ -2,8 +2,7 @@ class CreateMeetupRequests < ActiveRecord::Migration[5.0]
   def change
     create_table :meetup_requests do |t|
       t.string :zip
-      t.string :location
-      t.string :request
+      t.string :topic
     end
   end
 end
